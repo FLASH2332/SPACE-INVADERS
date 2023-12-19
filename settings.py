135bcf1a3ff475efbbc3b56e306720ee1912b7ba
FLASH2332 <148314235+FLASH2332@users.noreply.github.com>
2023-12-19 19:52:09 +0530

    Delete settings.py

--- settings.py
+++
@@ -1,7 +0,0 @@
-class Settings:
-    """A class to store all settings for Alien Invasion."""
-    def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (255,255,255)
-        self.ship_speed = 1.5
